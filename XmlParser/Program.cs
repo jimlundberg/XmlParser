4afeb91135b4a89720a8b5424bff529b3c9c40bb
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-07-05 09:14:12 -0700

    Add project files.

---
+++ XmlParser/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Xml;
+
+namespace XmlParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("C:\\Temp\\Test.xml");
+
+            XmlNode consumed = doc.DocumentElement.SelectSingleNode("/CONFIG_3155301-034/FileConfiguration/Consumed");
+            XmlNode produced = doc.DocumentElement.SelectSingleNode("/CONFIG_3155301-034/FileConfiguration/Produced");
+
+            Console.WriteLine("Consumed = " + consumed.InnerText);
+            Console.WriteLine("Produced = " + produced.InnerText);
+        }
+    }
+}
